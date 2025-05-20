245c5767c1e74f745c2fb8c2f6583f64c016bad4
thepeski <jpeski@icloud.com>
2025-05-20 22:39:06 +0200

    chore(repo): add path aliases

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,20 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ["babel-preset-expo"],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "@components": "./src/components",
+                        "@constants": "./src/constants",
+                        "@contexts": "./src/contexts",
+                        "@hooks": "./src/hooks",
+                        "@services": "./src/services",
+                        "@utils": "./src/utils"
+                    }
+                }
+            ]
+        ]
     };
 };
