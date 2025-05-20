54c993eeb60b3a26436fd1ce8ab2ace31584ca57
thepeski <jpeski@icloud.com>
2025-05-20 22:26:45 +0200

    feat(repo): expo router

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+/* _layout.tsx */
+// root layout
+
+// expo imports
+import { Slot } from "expo-router";
+
+function RootLayout() {
+    return <Slot />;
+}
+
+export default RootLayout;
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+/* index.tsx */
+// rendered by root layout
+
+// react imports
+import { SafeAreaView, Text } from "react-native";
+
+function App() {
+    return (
+        <SafeAreaView>
+            <Text>Hello world!</Text>
+        </SafeAreaView>
+    );
+}
+
+export default App;
